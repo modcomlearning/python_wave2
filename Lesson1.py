09862961e1b9c9b7a4c429dfebadeca18fff15bd
modcomlearning <modcomlearning@gmail.com>
2021-04-21 17:33:19 +0300

    First Commit

---
+++ Lesson1.py
@@ -0,0 +1,54 @@
+
+# we create numbers
+# we look at int, floats data types
+# int - whole numbers +/-
+# floats - numbers with decimals   +/-
+# variable - used to store values
+# Variable syntax  -  variable_name = value
+# Rules when coming up with variable name like x, y, z
+# R1: Use a meaningful word
+# R2: do not ue spaces in a a variable name, use underscore
+# R3: variable names are CASE SENSITIVE  ,  Max, MAX, max are different
+# R4: do not start with a number  , ... 1age not okay , age1 is oaky
+x = 4
+y = 5
+z = 10
+height = 1.5
+weight = 69.5
+basic_salary = 40000
+monthly_tax_nov = 12.5
+
+
+print(z)
+print(x)
+print(y)
+print(height)
+print(weight)
+print(basic_salary)
+print(monthly_tax_nov)
+
+# -----------------------------------
+# create a simple interest calc
+principle = 40000
+rate = 4.5
+time = 36
+
+# calculate interest
+interest = (principle * time * rate)/100
+print('Your Interest is ', interest, 'KES')
+print('Thank you')
+
+
+# Create a BMI calculator
+
+
+
+
+
+
+
+
+
+
+
+
