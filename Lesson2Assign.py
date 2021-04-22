fac531ae5281d35b0500b8f1acfbf157d97a90ab
modcomlearning <modcomlearning@gmail.com>
2021-04-22 16:41:03 +0300

    Second Commit

---
+++ Lesson2Assign.py
@@ -0,0 +1,10 @@
+
+
+
+# Create a program to find the area of a circle
+# Formula: PI Radius Squared
+# 3 mins
+radius = float(input('Enter radius: '))
+area = 3.14 * radius * radius
+print('Area is ', area)
+
