34b77582fe4b143b3acf0254e98d8add5e868ac9
modcomlearning <modcomlearning@gmail.com>
2021-04-22 16:59:46 +0300

    Second Commit

---
+++ Lesson2b.py
@@ -0,0 +1,18 @@
+
+
+marks = float(input('What is your marks'))
+# Operations in variables
+# 1. Arithmetic,  + , - , / , *
+# 2. Decision making:
+#    - > , >=, <, <=, !=
+if marks > 400:
+    print('You go to a national school')
+
+else:
+    print('You go to a provincial school')
+
+
+
+
+
+
