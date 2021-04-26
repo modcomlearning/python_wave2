d5302fa55fa352575f7c72992bf40f7d132620d8
modcomlearning <modcomlearning@gmail.com>
2021-04-26 16:29:34 +0300

    Third Commit

---
+++ Lesson3c.py
@@ -0,0 +1,14 @@
+
+# How about when we have 2 or more variables
+num1 = int(input('Enter random number1'))
+num2 = int(input('Enter Another Random Number2'))
+
+# Which is larger? or smaller
+if num1 > num2:
+    print('Num1 is Greater')
+
+elif num1 == num2:
+    print('The two numbers are same')
+
+else:
+    print('Num2 is Greater')  # this will execute if the other 2 are false
